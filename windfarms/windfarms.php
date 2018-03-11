6dcdadfe7c218eb59272f32190521ebe6fa2251f
Kiran Dangol <kiran_dan2009@yahoo.com>
2018-03-10 21:11:09 -0600

    Added new content type WindFarm. Small bug exists with field_create_field and field_create_instance which is why they're commented for now.

--- windfarms/windfarms.php
+++ windfarms/windfarms.php
@@ -1,172 +1,172 @@
 <?php
 
+
 /**
  * @file
- * Custom functionality for a wind farm database.
+ * Wind Farms installation.
  */
- 
- /**
- * Implements hook_help().
- */
- function windfarm_help($path, $arg){
-    switch ($path) {
-        case 'admin/help#windfarms':
-            {
-                $ret_val = '<h3>' . t('About') . '</h3>';
-                $ret_val .= '<p>' . t('The Wind Farms module makes it easy to manage a database of wind farms.') . '</p>';
-                return $ret_val; 
-                break;
-            }
-        }
- }
 
 /**
- * Implements hook_permission().
+ * Implements hook_install().
  */
- function windfarms_permission(){
-    return array(
-        'administer wind farms' => array(
-        'title' => t('Administer Wind Farms'),
-        'description' => t('Perform Administrative tasks on Wind Farms functionality'),
-    )
+function windfarms_install(){
+    // Set default variables.
+    variable_set('windfarms_gmap', 1);
+    variable_set('windfarms_default_center_lat', 42.91455);
+    variable_set('windfarms_default_center_long', -75.569851);
+    variable_set('windfarms_default_gmap_zoom', 8);   
+    
+    // Get localization for installation as t() may be unavailable.
+    $t = get_t();
+    
+    // Give user feedback.
+    drupal_set_message($t('Wind Farms variables created.'));
+    
+    // Content type definition.
+    $content_type = array(
+        'type' => 'windfarm',
+        'name' => $t('Wind Farm'),
+        'description' => $t('A Wind Farm, including location.'),
+        'title_label' => $t('Facility Name'),
+        'base' => 'node_content',
+        'custom' => TRUE,
     );
- }
- 
- /**
- * Implements hook_menu().
- */
- function windfarms_menu(){
-     $items = array();
-    // Admin configuration group.     
-     $items['admin/config/windfarms'] = array(
-         'title' => 'Wind Farms',
-         'description' => 'Administer Wind Farms', 
-         'access arguments' => array('administer wind farms'),
-     );
-     
-    // Admin configuration - Settings.
-    $items['admin/config/windfarms/manage'] = array(
-        'title' => 'Wind Farms Settings',
-        'description' => 'Manage Wind Farm settings and configurations.',
-        'access arguments' => array('administer wind farms'),
-        'page callback' => 'drupal_get_form',
-        'page arguments' => array('windfarms_admin_settings_form'),
+    
+    // Set remaining definitions with defaults.
+    $node_type = node_type_set_defaults($content_type);  
+    
+    
+    // Save the content type.
+    node_type_save($node_type);
+    
+    // Add a field for the body.
+    node_add_body_field($node_type, $t('Description'));
+    
+    // Create Fields.
+    $fields = array();
+        
+    $fields['windfarm_latitude'] = array(
+        'field_name' => 'windfarm_latitude',
+        'type' => 'number_float',
+        'settings' => array(
+            'max_length' => 20,
+        )
     );
-     
-    return $items;
- }
-
-/**
- * Implements hook_form().
- */
- function windfarms_admin_settings_form($node, &$form_state){
-     $form = array();
-     
-     $form['overview'] = array(
-         '#markup' => t('This interface allows administrators to manage general Wind Farm Settings'), 
-         '#prefix' => '<p>', 
-         '#suffix' => '</p>',          
-     );
-     
-     $form['windfarms_gmap'] = array(
-         '#title' => t('Enable Google Maps'),
-         '#description' => t('When enabled, Google Maps will be rendered if latitude and longitude are known.'),
-         '#type' => 'checkbox',
-         '#default' => variable_get('windfarms_gmap'),
-     );
-     
-     $form['default_center'] = array(
-         '#title' => t('Map center'),
-         '#description' => t('Location of the center of the map of wind farms.'),
-         '#type' => 'fieldset',
-         '#collapsible' => TRUE,
-         '#collapsed' => FALSE
-     );
-     
-     $form['default_center']['windfarms_default_center_lat'] = array(
-        '#title' => t('Latitude'),
-        '#description' => t('Signed degrees fromat (DDD.dddd)'),
-        '#type' => 'textfield',
-        '#default_value' => variable_get('windfarms_default_center_lat'),
-        '#required' => TRUE,
-     );
-     
-     $form['default_center']['windfarms_default_center_long'] = array(
-        '#title' => t('Longitude'),
-        '#description' => t('Signed degrees fromat (DDD.dddd)'),
-        '#type' => 'textfield',
-        '#default_value' => variable_get('windfarms_default_center_long'),
-        '#required' => TRUE,
-     );
-         
-     $options = range(0, 20, 1);
-     $options[0] = t('0 - Furthest');
-     $options[20] = t('20 - Furthest');
-     
-     $form['windfarms_default_gmap_zoom'] = array(
-         '#title' => t('Google Map Zoom'),
-         '#description' => t('Default level of zoom, between 0 and 20.'),
-         '#type' => 'select',
-         '#options' => $options,
-         '#default_value' => variable_get('windfarms_default_gmap_zoom'),
-         '#required' => TRUE,
-     );
-     
-     return system_settings_form($form);
-     
-//     $form['submit'] = array(
-//        '#type' => 'submit',
-//        '#value' => t('Save'),
-//     );
-         
-//     return $form;
- }
-
-/**
- * Validate Wind Farm admin settings.
- */
-function windfarms_admin_settings_form_validate($form, &$form_state){
-//  dpm($form_state['values']);
     
-    // Regular expression for validating signed degrees.
-    $signed_degree_regex = '/^[+]?\d+(\.\d+)?$/';
+    $fields['windfarm_longitude'] = array(
+        'field_name' => 'windfarm_longitude',
+        'type' => 'number_float',
+        'settings' => array(
+            'max_length' => 20,
+        )
+    );
     
-    // Shorthand for long array names.
-    $lat = $form_state['values']['windfarms_default_center_lat'];
-    $long = $form_state['values']['windfarms_default_center_long'];
+    $fields['windfarm_turbine_manufacturer'] = array(
+        'field_name' => 'windfarm_turbine_manufacturer',
+        'type' => 'text',
+        'settings' => array(
+            'max_length' => 60,
+        )
+    );
     
-    if(!preg_match($signed_degree_regex, $lat)) {
-        form_set_error('windfarms_default_center_lat', t('Invalid latitude; must be a signed degree (DD.dddd).'));
-    }
+    $fields['windfarm_unit_count'] = array(
+        'field_name' => 'windfarm_unit_count', 
+        'type' => 'number_integer',
+        'cardinality' => 1,
+        'settings' => array(
+            'max_length' => 5,
+        )
+    );
     
-    if(!preg_match($signed_degree_regex, $long)) {
-        form_set_error('windfarms_default_center_long', t('Invalid longitude; must be a signed degree (DD.dddd).'));
-    }
+//    foreach ($fields as $field){
+//        field_create_field($field);
+//    }
     
-    // Validate latitude and longitude values.
-    if(!((-180 <= $lat) && ($lat <= 180))){
-        form_set_error('windfarms_default_center_lat', t('Latitude must be between -180 and +180'));
-    }
+    // Create Field Instances.
+    $instances = array();
     
-    if(!((-180 <= $long) && ($long <= 180))){
-        form_set_error('windfarms_default_center_long', t('Longitude must be between -180 and +180'));
-    }
+    $instances['windfarm_unit_count'] = array(
+        'field_name' => 'windfarm_unit_count',
+        'label' => $t('Number of Units'),
+        'description' => $t('Number of individuals units at a given Facility'),
+        'widget' => array(
+            'type' => 'text_textfield',
+        ),
+        'required' => TRUE,
+        'settings' => array(
+            'text_processing' => 0,
+        ),
+    );
+    
+    
+    $instances['windfarm_latitude'] = array(
+        'field_name' => 'windfarm_latitude',
+        'label' => $t('Latitude'),
+        'description' => $t('Signed degrees format (DD.ddd)'),
+        'widget' => array(
+            'type' => 'text_textfield',
+        ),
+        'settings' => array(
+            'text_processing' => 0,
+        ),
+        'display' => array(
+            'default' => array(
+                'type' => 'hidden',  
+            ),            
+        ),
+    );
+    
+    $instances['windfarm_longitude'] = array(
+        'field_name' => 'windfarm_longitude',
+        'label' => $t('Longitude'),
+        'description' => $t('Signed degrees format (DD.ddd)'),
+        'widget' => array(
+            'type' => 'text_textfield',
+        ),
+        'settings' => array(
+            'text_processing' => 0,
+        ),
+        'display' => array(
+            'default' => array(
+                'type' => 'hidden',  
+            ),            
+        ),
+    );
+    
+    $instances['windfarm_turbine_manufacturer'] = array(
+        'field_name' => 'windfarm_turbine_manufacturer',
+        'label' => $t('Turbine Manufacturer'),
+        'description' => $t('The name of the turbine manufacturer'),
+        'widget' => array(
+            'type' => 'text_textfield',
+        ),
+        'display' => array(
+            'default' => array(
+                'label' => 'inline',
+            ),            
+        ),
+    );
+    
+//    foreach ($instances as $instance){
+//        $instance['entity_type'] = 'node';
+//        $instance['bundle'] = 'windfarm';
+//        field_create_instance($instance);
+//    }
     
 }
 
 /**
- * Process a validated Wind Farm admin setting submission.
+ * Implements hook_uninstall().
  */
-//function windfarms_admin_settings_form_submit($form, &$form_state){
-//    // Rebuild the form
-//    $form_state['rebuild'] = TRUE;
-//    
-//    // Save Wind Farm setting variables.
-//    variable_set('windfarms_gmap', $form_state['values']['windfarms_gmap']);
-//    variable_set('windfarms_default_center_lat', $form_state['values']['windfarms_default_center_lat']);
-//    variable_set('windfarms_default_center_long', $form_state['values']['windfarms_default_center_long']);
-//    variable_set('windfarms_default_gmap_zoom', $form_state['values']['windfarms_default_gmap_zoom']);
-//    
-//    drupal_set_message(t('Wind Farm settings saved.'));
-//}
-// 
+
+function windfarms_uninstall(){
+    // Delete variables.
+    variable_del('windfarms_gmap');
+    variable_del('windfarms_default_center_lat');
+    variable_del('windfarms_default_center_long');
+    variable_del('windfarms_default_gmap_zoom');
+    
+    // Inform user of removal.
+    $t = get_t();
+    drupal_set_message($t('Wind Farms variables removed.')); 
+}
